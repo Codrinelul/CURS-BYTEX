13dc637fae25f608119f6a7209af1f33a169a159
Squirtle420 <sandu.codrin2002@gmail.com>
2022-03-10 02:55:47 +0200

    2 prob

--- teme/pokedex/src/components/Card/Card.js
+++ teme/pokedex/src/components/Card/Card.js
@@ -7,30 +7,30 @@ const Card = ({ id, image, name, type, typeTwo, _callback }) => {
     const style = type + " card";
     const styleTwo = typeTwo + " card";
     return (
-        <div className="pokemonContainer" >
 
-            <div className={`cardContainer ${style}`}>
 
-                <div className="cardHeader">
-                    <h3>{name}</h3>
-                    <h4>#0{id}</h4>
-                </div>
-                <div className="cardBody">
-                    <div className="pokemonTypesContainer">
-                        <div className={`pokemonType ${style}`} >
-                            {type}
-                        </div>
-                        <div className={`pokemonType ${styleTwo}`} >
-                            {typeTwo}
-                        </div>
-                    </div>
-                    <div className="pokemonPhoto">
-                        <img src={image} alt={name} />
-                    </div>
+        <div className={`cardContainer ${style}`}>
 
-                </div>
+            <div className="cardHeader">
+                <h3>{name}</h3>
+                <h4>#0{id}</h4>
             </div>
-        </div >
+            <div className="cardBody">
+                <div className="pokemonTypesContainer">
+                    <div className={`pokemonType ${style}`} >
+                        {type}
+                    </div>
+                    <div className={`pokemonType ${styleTwo}`} >
+                        {typeTwo}
+                    </div>
+                </div>
+                <div className="pokemonPhoto">
+                    <img src={image} alt={name} />
+                </div>
+
+            </div>
+        </div>
+
     );
 };
 
