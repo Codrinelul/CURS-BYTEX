2a0c2552579074b8d76041b2638f60fcd70e84d0
Squirtle420 <sandu.codrin2002@gmail.com>
2022-03-22 15:41:36 +0200

    34 hours

---
+++ teme/pokedex/src/components/Filters/Filters.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class Filters extends React.Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     // console.log("props");
+    //     // console.log(this.props.data);
+    //     // this.state = {
+    //     //     showBook: true,
+    //     //     imgsrc: String
+    //     // }
+    // }
+    render() {
+        return (
+            <>
+                <div className="filter__container noselect">
+
+                    <div className="filter__items">
+                        <div>
+                            Type
+                        </div>
+                        <select value={this.props.valuetype} onChange={this.props.typesSelect}>
+                            {this.props.types.map((type) => (
+                                <option
+                                    key={type}
+                                    value={type}>{type}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="filter__items">
+                        <div>
+                            Sort by
+                        </div>
+                        <select value={this.props.sorttype} onChange={this.props.sortSelect}>
+                            {this.props.sortby.map((sort) => (
+                                <option
+                                    key={sort}
+                                    value={sort}>{sort}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="filter__items">
+                        <label>
+                            Search
+                        </label>
+                        <input type="text" value={this.props.valuesearch} onChange={this.props.searchChange} />
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Filters;
---
+++ teme/pokedex/src/components/Header/Header.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Pokedex from "./pokedex.png";
+
+class Header extends React.Component {
+
+    changeTheme = () => {
+
+        // debugger
+        const currentTheme = document.documentElement.getAttribute('data-theme');
+        // console.log(currentTheme);
+
+        let targetTheme = "dark";
+
+        if (currentTheme === "dark") {
+            targetTheme = "light";
+
+            this.setState({
+                isChecked: true,
+            })
+
+            // console.log(targetTheme);
+        } else {
+            this.setState({
+                isChecked: false,
+            })
+        }
+        document.documentElement.setAttribute('data-theme', targetTheme)
+    }
+
+
+
+    render() {
+        return (
+            <>
+                <div className="app__header">
+
+                    <div className="poke__logos noselect">
+                        <img src={Pokedex} alt="pokelogo" className="poke__logo" />
+
+                    </div>
+                    <div className="switch">
+
+                        <div className="toggle">
+                            <label htmlFor="themeSwitch"></label>
+                            <input type="checkbox" name="swich-theme" id="themeSwitch" onClick={this.changeTheme} defaultChecked />
+                            <div className="toggle-bg"></div>
+                            <div className="toggle-thumb">
+                                <i className="fas fa-sun"></i>
+                                <i className="fas fa-moon"></i>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Header;
